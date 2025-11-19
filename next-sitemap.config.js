30c2877b4b2d02c4458bfa871d1b95f13c426de5
amanpandey2101 <paman7681@gmail.com>
2025-11-19 23:53:12 +0530

    Bug Fix

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: "https://tinyur.in", 
-    generateRobotsTxt: true, 
-    sitemapSize: 7000,
-    changefreq: "daily",
-    priority: 0.7,
-  };
-  
